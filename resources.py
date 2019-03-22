715ddd110e971a42f9a92263bb2d16492157c4f4
sadmonad <sadmonad@gmail.com>
2019-03-22 17:19:27 +0300

    Added check for user uniqueness during post

--- resources.py
+++ resources.py
@@ -1,3 +1,7 @@
+import falcon
+from falcon import HTTPConflict
+from sqlalchemy.exc import IntegrityError
+
 from configs import SQLAlchemyConfig as db
 from models import User
 from schemas import users_schema, user_schema
@@ -16,5 +20,9 @@ class UserResource:
     def on_post(self, req, resp):
         json = req.media
         new_user = user_schema.load(json, session=db.SESSION).data
-        foo = db.SESSION.add(new_user)
-        resp.media = foo
+        try:
+            db.SESSION.add(new_user)
+            db.SESSION.commit()
+        except IntegrityError:
+            raise HTTPConflict('Unique contraint failed')
+        resp.status = falcon.HTTP_201
